6d15e09fe48f238cfd8b16ab8b6bdde47ff0ec81
Awood549 <Austin.wood1776@live.com>
2021-02-02 09:31:20 -0800

    scrapping nextjs Links for the moment and using <a> tag

--- portfolio/pages/navBar.js
+++ portfolio/pages/navBar.js
@@ -4,17 +4,17 @@ function NavBar() {
   let links =[]
 
   return (
-
+    <>
     <ul>
       <li> 
-        <Link href= '/projects'> <a> Projects </a> </Link> 
+      <a href= '/projects'> Projects </a>
         </li>
 
       <li> About Me</li>
       <li> Other thing</li>
       <li> Other Other Thing</li>
     </ul>
-
+</>
   )
 }
 
