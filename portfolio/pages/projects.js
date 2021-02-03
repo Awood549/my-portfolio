85543622bf22bd0b4f4e5a106197b0995a39c9d8
Awood549 <Austin.wood1776@live.com>
2021-02-03 10:18:25 -0800

    adding projects to project page state

--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -1,11 +1,22 @@
 import Header from '../components/header.js'
 
-export default function Projects () {
+export default function Projects (props) {
+  let prjoects = [
+    {
+      name: 'Guess Who',
+      link: 'www.whodat.com',
+      description: 'find tha dude',
+      techDesc: 'using a keyboard I Blah',
+      
+    }
+  ]
 
   return(
     <>
     <Header> </Header>
     <h2> Hur Da Projects </h2>
+
+
 
     </>
   )
