9b97f8981a6e7ae5fd9f503c685fe6046acbf55f
Awood549 <Austin.wood1776@live.com>
2021-02-03 10:27:07 -0800

    building framework for project component

--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -1,19 +1,29 @@
 import Header from '../components/header.js'
 
 export default function Projects (props) {
-  let prjoects = [
+  let projects = [
     {
       name: 'Guess Who',
       link: 'www.whodat.com',
       description: 'find tha dude',
       techDesc: 'using a keyboard I Blah',
+      thumbNail: 'Pic',
       
     }
   ]
 
+  function displayProject(object){
+    return <>
+    <h4> {object.name} </h4>
+    <image></image>
+    </>
+  }
+                   
   return(
     <>
     <Header> </Header>
+    {displayProject(projects[0])}
+
     <h2> Hur Da Projects </h2>
 
 
