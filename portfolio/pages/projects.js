c3de4eee70115d646dad53679182bd176133bfbc
Awood549 <Austin.wood1776@live.com>
2021-02-08 10:46:56 -0800

    trouble looping through the projects array, feels close.

--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -10,6 +10,14 @@ export default function Projects (props) {
       techDesc: 'using a keyboard I Blah',
       // thumbNail: GuessWhoThumb,
       
+    },
+    {
+      name: 'Guess Who',
+      link: 'mean-gorls.github.io/guess-who/',
+      description: 'find tha dude',
+      techDesc: 'using a keyboard I Blah',
+      // thumbNail: GuessWhoThumb,
+      
     }
   ]
 
@@ -20,13 +28,29 @@ export default function Projects (props) {
     <Image src = '/GuessWhoThumb.PNG' width='100%' height = '100%'/>
     </>
   }
+
+  function showAllProjects(array){
+    
+
+    const list = <> <ul>
+
+    {array.map((project,idx) => {
+      <li key={idx}> {console.log('ping')
+    }'HEYYYY'</li>
+    })}
+    </ul>
+    </>
+
+    return list
+  }
                    
   return(
     <>
     <Header> </Header>
-    {displayProject(projects[0])}
+    {/* {displayProject(projects[0])} */}
 
     <h2> Hur Da Projects </h2>
+    {showAllProjects(projects)}
 
 
 
