4fccbb37944d3edc4c0b5e7d1451ff65cbf82442
Awood549 <Austin.wood1776@live.com>
2021-03-23 09:31:17 -0700

    about me updated

--- portfolio/pages/aboutMe.js
+++ portfolio/pages/aboutMe.js
@@ -7,6 +7,19 @@ function AboutMe() {
     <Header></Header>
     <h2> Heres the stuff about me! </h2>
 
+    <h3> *Came From* Non-Traditional Background</h3>
+    <p>Started from the boonies and now im here. <br></br> education came from </p>
+
+    <h3>  What am I doing now?</h3>
+    <p> building freelance projects and fixing bugs for strangers while searching for my company to nurture <br> </br>*at least one link to projects*</p>
+
+    <h3> Where will I be in 5 years? </h3>
+    <p> *find my company and make it my home, Inclusionary focus*</p>
+
+    <h3> Bonus for panel for the people who want to know the kind of person I am</h3>
+    <p> I spend my free time in a balance between my cat, my partner, my books, and my laptop. </p>
+
+
 </>
   )
 }
